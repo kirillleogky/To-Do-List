0e2a49a0046a3bbf22959be926213032835a9ccf
Kirill Leogky <kirillmargheriti@gmail.com>
2020-06-04 17:47:17 +0300

    feat: add basic markup of the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from "react"
+import "./App.css"
+import Overlay from "./components/overlay/overlay"
+import AddTodoInput from "./components/addTodoInput/addTodoInput"
+import AddTodos from "./components/todos/addTodos"
+import Navigation from "./components/navigation/navigation"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    <Fragment>
+      <div id="todo" className="todo_block">
+        <h1 className="todo_block-title">
+          To Do List
+          <i id="pensil" className="todo_block-title_img"></i>
+        </h1>
+        <AddTodoInput />
+        <AddTodos />
+        <Navigation />
+      </div>
+      <Overlay />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
