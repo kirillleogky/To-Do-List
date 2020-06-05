ab0c43f3a2dac78da50f771e24e33e8966467800
Kirill Leogky <kirillmargheriti@gmail.com>
2020-06-05 16:07:18 +0300

    refactor: rename "todos" files

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 export default function addTodos() {
   return (
@@ -12,5 +12,5 @@ export default function addTodos() {
         <span className="todos_block-trash"></span>
       </li>
     </ul>
-  )
+  );
 }
