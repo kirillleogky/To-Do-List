becaa17e7d52d9b4fbbb37c8137d8ba08efbf138
Kirill Leogky <kirillmargheriti@gmail.com>
2020-06-06 14:41:44 +0300

    feat: rename todos component, delete unnecessary features
    rename todos component for better looking, remove the use of redux in 
    the component and parts of code designed to test redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Fragment } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import actions from "./actions";
 import "./App.scss";
 import Overlay from "./components/overlay/overlay";
 import AddTodoInput from "./components/addTodoInput/addTodoInput";
-import AddTodos from "./components/todos/addTodos";
+import AddTodos from "./components/todos/todos";
 import Navigation from "./components/navigation/navigation";
 
-function App(props) {
+export default function App() {
   return (
     <Fragment>
       <div id="todo" className="todo_block">
         <h1 className="todo_block-title">
           To Do List
-          <i id="pensil" className="todo_block-title_img">
-            by {props.firstData}
-          </i>
-          <button
-            type="button"
-            onClick={() => props.setFirstData("Kirillio")}
-          ></button>
+          <i id="pensil" className="todo_block-title_img"></i>
         </h1>
         <AddTodoInput />
         <AddTodos />
@@ -30,28 +21,3 @@ function App(props) {
     </Fragment>
   );
 }
-
-App.propTypes = {
-  firstData: PropTypes.string,
-  secondData: PropTypes.object,
-  thirdData: PropTypes.array,
-  setFirstData: PropTypes.func,
-};
-
-const mapStateToProps = (store) => {
-  return {
-    firstData: store.firstData.data,
-    secondData: store.secondData.data,
-    thirdData: store.thirdData.data,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setFirstData: (data) => dispatch(actions.setFirstData(data)),
-    setSecondData: (data) => dispatch(actions.setSecondData(data)),
-    setThirdData: (data) => dispatch(actions.setThirdData(data)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
