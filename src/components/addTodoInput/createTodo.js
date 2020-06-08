d816442751d24e5b57cf82dbeb1adf8277e8a000
Kirill Leogky <kirillmargheriti@gmail.com>
2020-06-08 14:05:16 +0300

    feat: add functionality removal cases from the list

--- src/components/addTodoInput/createTodo.js
+++ src/components/addTodoInput/createTodo.js
@@ -1,18 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import actions from "../../actions";
 
 function createTodo(props) {
+  function deleteTodo(text) {
+    const delIndex = props.thirdData.findIndex((todo) => todo.label === text);
+    const todoStart = props.thirdData.slice(0, delIndex);
+    const todoEnd = props.thirdData.slice(delIndex + 1);
+    props.setThirdData([...todoStart, ...todoEnd]);
+  }
   return props.thirdData.map((elem, index) => (
     <li key={index}>
       <span className="todos_block-text">{elem.label}</span>
-      <span className="todos_block-trash"></span>
+      <button
+        className="todos_block-trash"
+        onClick={() => deleteTodo(elem.label)}
+      >
+        x
+      </button>
     </li>
   ));
 }
 
 createTodo.propTypes = {
   thirdData: PropTypes.array,
+  setThirdData: PropTypes.func,
 };
 
 const mapStateToProps = (store) => {
@@ -21,4 +34,10 @@ const mapStateToProps = (store) => {
   };
 };
 
-export default connect(mapStateToProps)(createTodo);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setThirdData: (data) => dispatch(actions.setThirdData(data)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(createTodo);
